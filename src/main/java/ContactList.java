241a2ab851b0958449b3f1df400d2e1f4f974f99
nathanielbarad <nate.barad@menloschool.org>
2026-01-22 16:34:30 -0800

    finished sort and search methods

--- src/main/java/ContactList.java
+++ src/main/java/ContactList.java
@@ -33,9 +33,65 @@ public class ContactList {
                            "\n8. Search By Phone Number" +
                            "\n0. Exit");
     }
-public void printContacts() {
+    public void printContacts() {
         for (Person p : contacts) {
             System.out.println(p);
         }
     }
+    public void sort(int sortBy) {
+        if (sortBy == 0) {
+            for (int i = 0; i < contacts.size(); i++) {
+                for (int j = 0; j < i - 1; j++) {
+                    if (contacts.get(j).getLastName().compareTo(contacts.get(j + 1).getLastName()) > 0) {
+                        contacts.add(j, contacts.remove(j + 1));
+                    }
+                }
+            }
+        }
+        else if (sortBy == 1) {
+            for (int i = 0; i < contacts.size(); i++) {
+                for (int j = 0; j < i - 1; j++) {
+                    if (contacts.get(j).getFirstName().compareTo(contacts.get(j + 1).getFirstName()) > 0) {
+                        contacts.add(j, contacts.remove(j + 1));
+                    }
+                }
+            }
+        }
+        else if (sortBy == 2) {
+            for (int i = 0; i < contacts.size(); i++) {
+                for (int j = 0; j < i - 1; j++) {
+                    if (contacts.get(j).getPhoneNumber().compareTo(contacts.get(j + 1).getPhoneNumber()) > 0) {
+                        contacts.add(j, contacts.remove(j + 1));
+                    }
+                }
+            }
+        }
+    }
+
+    public Person searchByFirstName(String firstName) {
+        for (Person p : contacts) {
+            if (p.getFirstName().equals(firstName)) {
+                return p;
+            }
+        }
+        return null;
+    }
+■   public Person searchByLastName(String lastName) {
+        for (Person p : contacts) {
+            if (p.getLastName().equals(lastName)) {
+                return p;
+            }
+        }
+        return null;
+    }
+■   public Person searchByPhoneNumber(String phoneNumber) {
+        for (Person p : contacts) {
+            if (p.getPhoneNumber().equals(phoneNumber)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    
 }
